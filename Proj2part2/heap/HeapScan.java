d2ad4814f885060c917ea5fa92d5f9b820dd7b42
Ojas <ojjsaw@gmail.com>
2013-03-30 13:03:57 -0400

    basic project

---
+++ Proj2part2/heap/HeapScan.java
@@ -0,0 +1,50 @@
+package heap;
+
+import global.RID;
+
+public class HeapScan {
+	
+	/**
+
+	* Constructs a file scan by pinning the directoy header page and initializing
+
+	* iterator fields.
+
+	*/
+
+	protected HeapScan(HeapFile hf) {}
+
+
+	protected void finalize() throws Throwable {}
+
+	/**
+
+	* Closes the file scan, releasing any pinned pages.
+
+	*/
+	public void close() {}
+
+
+	/**
+
+	* Returns true if there are more records to scan, false otherwise.
+
+	*/
+	public boolean hasNext() {
+		return false;}
+
+	 
+
+	/**
+
+	* Gets the next record in the file scan.
+
+	* @param rid output parameter that identifies the returned record
+
+	* @throws IllegalStateException if the scan has no more elements
+
+	*/
+	public Tuple getNext(RID rid) {
+		return null;}
+	
+}
