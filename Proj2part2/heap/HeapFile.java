8faf3c11a2ab30c221692150c57552278a7975da
Ojas <ojjsaw@gmail.com>
2013-03-31 21:23:40 -0400

    barely added the first function in heapfile.java... working on buffermanager... will switch from buffermanager to heap in 2 hrs...

--- Proj2part2/heap/HeapFile.java
+++ Proj2part2/heap/HeapFile.java
@@ -4,23 +4,59 @@ import chainexception.ChainException;
 import heap.HFPage;
 
 import global.GlobalConst;
+import global.Minibase;
+import global.Page;
 import global.PageId;
 import global.RID;
 
 public class HeapFile {
 
+	PageId hpid;
 
 /**
    * If the given name already denotes a file, this opens it; otherwise, this
    * creates a new empty file. A null name produces a temporary heap file which
    * requires no DB entry.
    */
-  public HeapFile(String name) {}
+  public HeapFile(String name) {
+	  
+	  PageId pageno = null;
+	  Page page;
+	  HFPage hfpage;
+	  
+	  pageno = Minibase.DiskManager.get_file_entry(name);
+	  
+	  if(pageno == null) {
+		  pageno = new PageId();
+		  page = new Page();
+		  pageno = Minibase.BufferManager.newPage(page, 1);
+		  
+		  Minibase.DiskManager.add_file_entry(name, pageno);
+		  
+		  //not sure if needed
+		  hfpage = new HFPage(page);
+		  
+	  }
+	  
+	  //TODO: open the file - openDB?  
+	  hpid = new PageId();
+	  hpid.pid = pageno.pid;
+  }
  
 /**
    * Deletes the heap file from the database, freeing all of its pages.
    */
-  public void deleteFile() {}
+  public void deleteFile() {
+	  
+	  /*
+	  delete heap file
+	  make sure all pages are freed
+	  */
+	  
+	  //HFPage page = new HFPage();
+	  
+	  
+  }
  
   /**
    * Inserts a new record into the file and returns its RID.
@@ -28,7 +64,13 @@ public class HeapFile {
    * @throws IllegalArgumentException if the record is too large
    */
   public RID insertRecord(byte[] record) throws ChainException{
-	return null;}
+	  
+	  /*
+	  insert new record in file
+	  return rid
+		*/	  
+	  return null;
+	}
  
   /**
    * Reads a record from the file, given its id.
@@ -36,7 +78,11 @@ public class HeapFile {
    * @throws IllegalArgumentException if the rid is invalid
    */
   public byte[] selectRecord(RID rid) {
-	return null;}
+	  
+	  //read record from the file based on id
+	  
+	return null;
+  	}
  
 /**
    * Updates the specified record in the heap file.
@@ -45,7 +91,11 @@ public class HeapFile {
    * @throws IllegalArgumentException if the rid or new record is invalid
    */
   public boolean updateRecord(RID rid, Tuple newTuple) throws ChainException{
-	return false;}
+	  
+	  //update record in heap file
+	  
+	return false;
+	}
  
   /**
    * Deletes the specified record from the heap file.
@@ -54,13 +104,19 @@ public class HeapFile {
    * @throws IllegalArgumentException if the rid is invalid
    */
   public boolean deleteRecord(RID rid) {
-	return false;}
+	  
+	  //delete record from heap file
+	return false;
+	}
  
 /**
    * Gets the number of records in the file.
    */
   public int getRecCnt() {
-	return 0;}
+	  
+	  //get no. of records in file
+	return 0;
+	}
  
   /**
    * Searches the directory for a data page with enough free space to store a
@@ -68,6 +124,11 @@ public class HeapFile {
    * data page.
    */
   protected PageId getAvailPage(int reclen) {
+	  
+	  //search directory for page with enough free space to store record of given size
+	  //if no suitable page found
+	  //create data page
+	  
 	return null;}
 
 public HeapScan openScan() {
